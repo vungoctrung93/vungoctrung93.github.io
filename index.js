2b91885c12cd304f130bd5747593eb8183025d41
Vu Ngoc Trung <vungoctrung93@gmail.com>
2022-03-28 17:18:45 +0700

    change title as function

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ function VVN(){
     document.getElementById("table").hidden = true;
     document.getElementById("VVN").hidden = false;
     document.getElementsByClassName("navbar-brand")[0].innerHTML = "FVC Đà Nẵng"
+    document.getElementsByTagName("title")[0].innerHTML = "FVC Đà Nẵng"
 }
 function PPI(){
     localStorage.setItem("selectedFunc", "PPI");
     document.getElementById("table").hidden = false;
     document.getElementById("VVN").hidden = true;
     document.getElementsByClassName("navbar-brand")[0].innerHTML = "Vũ Ngọc Trung"
+    document.getElementsByTagName("title")[0].innerHTML = "Vũ Ngọc Trung"
 }
