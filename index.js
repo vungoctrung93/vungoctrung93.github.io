dea1569a2378ec15fdb1413d61321d613777410e
Vu Ngoc Trung <vungoctrung93@gmail.com>
2022-03-28 17:05:17 +0700

    update brand name

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ function VVN(){
     localStorage.setItem("selectedFunc", "VVN");
     document.getElementById("table").hidden = true;
     document.getElementById("VVN").hidden = false;
+    document.getElementsByClassName("navbar-brand")[0].innerHTML = "FVC Đà Nẵng"
 }
 function PPI(){
     localStorage.setItem("selectedFunc", "PPI");
     document.getElementById("table").hidden = false;
     document.getElementById("VVN").hidden = true;
+    document.getElementsByClassName("navbar-brand")[0].innerHTML = "Vũ Ngọc Trung"
 }
